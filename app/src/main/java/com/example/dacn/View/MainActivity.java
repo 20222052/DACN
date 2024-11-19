6f5ea9b48487304df1c88dce86b24e83d064603c
thinh <thinhgiap76@gmail.com>
2024-11-19 15:54:27 +0700

    login_1

--- app/src/main/java/com/example/dacn/View/MainActivity.java
+++ app/src/main/java/com/example/dacn/View/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.dacn.View;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,10 +10,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.dacn.Controller.MainActivityController;
 import com.example.dacn.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_dk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        MainActivityController controller = new MainActivityController();
+        new MainActivityController();
+        btn_dk = findViewById(R.id.btn_dk);
+        btn_dk.setOnClickListener(view -> {
+            controller.DK_TaiKhoan(1, 1, "thinh", "123", "admin");
+        });
     }
 }
